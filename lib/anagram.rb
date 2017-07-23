548da085e6df06b37c3066bb0db0974ae3c999f0
mthorry <mthorry@gmail.com>
2017-07-23 18:10:08 -0400

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(arr)
+    arr.select {|word| word.split("").sort == @word.split("").sort}
+    # splits each into letters, sorts letters alphabetically
+    #splits word into individual letters and sorts them alphabetically
+    # iterates through array of alphabetized letters and checks to see if they match @word's letters
+    # returns the original word from array that is anagram of @word
+  end
+
+end
+
+# it 'should detect an anagram' do
+#   detector = Anagram.new('listen')
+#   listen = detector.match %w(enlists google inlets banana)
+#   expect(listen).to eq(['inlets'])
+# end
